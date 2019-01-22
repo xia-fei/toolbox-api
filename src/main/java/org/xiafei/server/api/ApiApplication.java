14e1d8d78b58b35c4e6b1adb7f5cb1ff931c0500
夏飞 <xf188866@alibaba-inc.com>
2019-01-22 11:59:00 +0800

    设置跨域支持

--- src/main/java/org/xiafei/server/api/ApiApplication.java
+++ src/main/java/org/xiafei/server/api/ApiApplication.java
@@ -2,13 +2,19 @@ package org.xiafei.server.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").maxAge(Long.MAX_VALUE);
+    }
 }
 
